bd2ce42aa91696f15c8d85a4f0e347ba01b3f758
Pavel Malyshev <pavel.v.malyshev@gmail.com>
2026-09-15 11:39:59 +0300

    modified:   vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_URL || "/",
-  //base: '/SudokuAI/', // Замените на имя вашего репозитория
+  //base: process.env.VITE_BASE_URL || "/",
+  base: '/SudokuAI/', // Замените на имя вашего репозитория
   server: {
     host: "0.0.0.0",
     port: 3000,
